b4db3a7d891662397dfe46dd158b5075a645fc99
inchic <poiuy1709@naver.com>
2019-08-14 17:38:15 +0900

    inchic

---
+++ src/main/java/com/example/springboot190814/controller/RestWSController.java
@@ -0,0 +1,21 @@
+package com.example.springboot190814.controller;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin
+@RestController
+public class RestWSController {
+    @RequestMapping(value = "/ws/organization")
+    public Object actionMethod() {
+        Map<String,Object> resultObject = new HashMap<String,Object>();
+        resultObject.put("Member_ID", "293029301202");
+        resultObject.put("CellPhone", "001-3923-6940");
+        resultObject.put("Age", "29");
+        resultObject.put("Password", "123123123");
+
+        return resultObject;
+    }
+}
